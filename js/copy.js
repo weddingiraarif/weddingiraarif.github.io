622bd532455b9b8c5f704f78ec9e5c8dfee2d907
weddingiraarif <khanzina150@gmail.com>
2022-11-13 10:20:46 +0700

    menambahkan dana

--- js/copy.js
+++ js/copy.js
@@ -11,3 +11,17 @@ function copyText() {
     // Alert the copied text
     alert("Nomer Rekening Sudah Tercopy");
 }
+
+function copyText1() {
+    // Get the text field
+    var copyText = document.getElementById("text-copy1");
+
+    // Select the text field
+    copyText.select();
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+
+    // Alert the copied text
+    alert("Nomer Dana Sudah Tercopy");
+}
